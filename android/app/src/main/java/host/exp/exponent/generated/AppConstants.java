77330cd462e1cf19b9d1cb4457164f6fe9f49e7b
Rudy Schmitz <rschmitz@pnmg.com>
2019-02-06 09:22:56 -0600

    1.0.3 Release

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -24,10 +24,11 @@ public class AppConstants {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
     
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@rwschmitz/surveys-for-days", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40rwschmitz%2Fsurveys-for-days%2F1.0.0%2F13c3e026b11ea65813978fd309f30430-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40rwschmitz%2Fsurveys-for-days%2F1.0.0%2F9d6f84f90e67b8bb2caa59c52cf2b267-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
